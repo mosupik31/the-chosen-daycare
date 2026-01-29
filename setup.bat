ca983fa9d5efb0bfd5607027b0036ca7c19d9e2f
mosupik31 <mosupik31@gmail.com>
2026-01-29 17:40:28 +0200

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,18 @@
+@echo off
+echo Creating The Chosen Day Care website structure...
+
+mkdir images
+mkdir .github\workflows
+
+echo Creating all files...
+REM Copy all the code files here from above
+
+echo.
+echo Setup complete!
+echo.
+echo Next steps:
+echo 1. Go to github.com and create repository: the-chosen-daycare
+echo 2. Upload all these files
+echo 3. Enable GitHub Pages in Settings
+echo.
+pause
